423a589b56d942e18137a4339b85af64d603ec8c
Raghuveera Betina <rbetina@gmail.com>
2012-07-08 21:53:45 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rxngif::Application.config.secret_token = '45441674e3ea32479038f1362e078fae3fa9bf1061d4ec2fe4b24ebdf839ef54cfa301b475cce842d22edd8c35b062217911aad84d0fd59be2b7187e216e55a4'
